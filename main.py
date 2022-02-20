dffa46b859af617b3bd61df28a4328d8f9c0dd25
KrishenK <fox2581kriskenk@gmail.com>
2022-02-20 19:17:29 +0100

    Host and Join created, need fix the winner part and some optimization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from graphics import start_menu
+from graphics import start_menu,multiplayer_menu, multiplayer_address_menu
 from local import local
 from init import grid
 from curses import wrapper
@@ -9,6 +9,12 @@ def main():
     while choice != 3:
         choice = wrapper(start_menu)
         if choice == 1: local(grid)
+        elif choice == 2: 
+            multichoice = -1
+            while multichoice != 3:
+                multichoice = wrapper(multiplayer_menu)
+                if multichoice == 1: wrapper(multiplayer_address_menu, 0, "127.0.0.1", "25565")
+                if multichoice == 2: wrapper(multiplayer_address_menu, 1)
         
 if __name__ == "__main__":
     main()
--- multiplayer.py
+++ multiplayer.py
@@ -1,16 +1,23 @@
 from curses import wrapper
+from pydoc import cli
 from graphics import local_play_screen
 from check import verify_grid
 from save import add_piece
+import socket
+import json
+
+from requests import get
+from save import load_grid
+# from init import grid
 
 
-def multiplayer(grid: list) -> None:
+def multiplayer(s: socket, turn: int, grid: list) -> None:
     """manages the local playing logic
 
     Args:
         grid (list): the four in a row grid
     """
-    winner, quit, turn = "", False, 1
+    winner, quit = ("", False)
     column = 0
     
     while winner == "" and not quit:
@@ -18,8 +25,79 @@ def multiplayer(grid: list) -> None:
         if column == -1: break
         add_piece(grid, turn, column)
         winner = verify_grid(grid)
-        if turn == 0: turn = 1
-        else: turn = 0
+        return (winner, grid)
+
+
+def host(host: str, port: int, grid: list):
+    backlog = 5
+    size = 1024
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(backlog)
+    print("Serveur allumé")
+    client, address = s.accept() # Waiting for client connection
+
+    client.send(json.dumps(grid).encode())
+
+    winner = ""
+
+    while not winner:
+        try:
+            data = client.recv(size)
+        except ConnectionResetError:
+            break
+
+        print(data)
+        if data.decode() != "win":
+            (winner, grid) = multiplayer(client, 1, json.loads(data.decode()))
+            client.send(json.dumps(grid).encode())
+
+            print(winner)
+
+            if winner == "yellow": client.send("win".encode())
+            else: client.send(json.dumps(grid).encode())
+        else:
+            winner = "red"
+
+
+    client.close()
+
+def join(host: str, port: int) -> None:
+    size = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect((host, port))
+        print("Connecté sur {}:{}".format(host, port))
+    except Exception as e:
+        print(e)
+
+    winner = ""
+
+    while not winner:
+        try:
+            data = s.recv(size)
+        except ConnectionResetError:
+            break
+
+        print(data)
+
+        if data.decode() != "win":
+            (winner, grid) = multiplayer(s, 0, json.loads(data.decode()))
+
+            if winner == "red": s.send("win".encode())
+            else: s.send(json.dumps(grid).encode())
+        else:
+            winner = "yellow"
+    s.close()
+
+
 
 if __name__ == "__main__":
-    multiplayer()
+    # print(get("https://api.ipify.org").text)
+
+    t = input('>>')
+    grid = load_grid('./grids/game1.txt')
+    if(t == 'join'): join('127.0.0.1', 25565)
+    else: host('127.0.0.1', 25565, grid)
