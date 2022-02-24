8de206c95a0cd0c9d094941bf61ceb1622f3bbce
KrishenK <fox2581kriskenk@gmail.com>
2022-02-25 00:23:03 +0100

    Important update about multiplayer mode

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import start_menu,multiplayer_menu, multiplayer_address_menu
 from local import local
+from multiplayer import host, join
 from init import grid
 from curses import wrapper
 
@@ -13,8 +14,8 @@ def main():
             multichoice = -1
             while multichoice != 3:
                 multichoice = wrapper(multiplayer_menu)
-                if multichoice == 1: wrapper(multiplayer_address_menu, 0, "127.0.0.1", "25565")
-                if multichoice == 2: wrapper(multiplayer_address_menu, 1)
+                if multichoice == 1: host(wrapper(multiplayer_address_menu, 0, "127.0.0.1", "25565"), grid)
+                if multichoice == 2: join(wrapper(multiplayer_address_menu, 1))
         
 if __name__ == "__main__":
     main()
