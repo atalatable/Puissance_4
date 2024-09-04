3085abae0fe1d2dd0e8ca2f31c91f45e3e6e8797
atalata <kenny.lutz@laposte.net>
2024-09-04 15:55:38 +0200

    Fix problematic issue (didnt test multiplayer)

--- init.py
+++ init.py
@@ -5,3 +5,7 @@ KEY_ESC = 27
 KEY_RETURN = 8
 KEY_NUM = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
 KEY_DOT = 46
+
+def get_new_grid():
+    return [[0]*7 for i in range(6)]
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graphics import start_menu,multiplayer_menu, multiplayer_address_menu
 from local import local
 from multiplayer import host, join
-from init import grid
+from init import get_new_grid
 from save import load_grid
 from curses import wrapper
 
@@ -10,12 +10,12 @@ def main():
     
     while choice != 3:
         choice = wrapper(start_menu)
-        if choice == 1: local(grid)
+        if choice == 1: local(get_new_grid())
         elif choice == 2: 
             multichoice = -1
             while multichoice != 3:
                 multichoice = wrapper(multiplayer_menu)
-                if multichoice == 1: host(wrapper(multiplayer_address_menu, 0), grid)
+                if multichoice == 1: host(wrapper(multiplayer_address_menu, 0), get_new_grid())
                 if multichoice == 2: join(wrapper(multiplayer_address_menu, 1))
         
 if __name__ == "__main__":
